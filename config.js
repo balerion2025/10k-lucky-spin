8b39f0ca0615cc03f4ab09e024c317dd2c8b6e2f
balerion2025 <varadilaszlo2024@gmail.com>
2026-05-04 14:20:13 +0200

    Use legacy anon key for PostgREST leaderboard

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SLOT_CONFIG = {
-  SUPABASE_URL: "https://nvnjjlbvihertbccyqjr.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_GCNIeoYJLpZdkXyELPGVAg_zCSnIDwm"
+  SUPABASE_URL: "https://nvnijlbvihertbccyqjr.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52bmlqbGJ2aWhlcnRiY2N5cWpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4NTkzMjUsImV4cCI6MjA5MzQzNTMyNX0.adAC_qA1Vmfv-gX_UAwD78nX-MndElT0pCVQynut1e0"
 };
